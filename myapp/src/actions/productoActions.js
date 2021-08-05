477c9271b0b49d73c62eb9cf1f452071a3ba55f3
Lautaro <lautarosoliani@hotmail.com>
2021-08-04 23:02:46 -0300

    300 f

--- myapp/src/actions/productoActions.js
+++ myapp/src/actions/productoActions.js
@@ -1,4 +1,11 @@
 export const AGREGAR_PRODUCTO = "AGREGAR_PRODUCTO"
 export const AGREGAR_PRODUCTO_EXITO = "AGREGAR_PRODUCTO_EXITO"
 export const AGREGAR_PRODUCTO_ERROR = "AGREGAR_PRODUCTO_ERROR"
-export const AGREGAR_PRODUCTO_ERROR = "AGREGAR_PRODUCTO_ERROR"
+
+// crear nuevos productos
+
+export function crearNuevoProductoAction() {
+    return () => {
+        console.log("desde actio")
+    }
+}
--- myapp/src/components/NuevoProducto.js
+++ myapp/src/components/NuevoProducto.js
@@ -1,6 +1,23 @@
 import React from "react"
+//Actions de REDUX
+import { useDispatch, useSelector } from "react-redux"
+import { crearNuevoProductoAction } from "../actions/productoActions"
 
 const NuevoProducto = () => {
+    //utilizar useDispatch y te crea una funcion
+    const dispatch = useDispatch()
+    //manda llamar el action de productoAction
+    const agregarProducto = () => dispatch(crearNuevoProductoAction())
+    //cuando el ususario haga submit
+    const submitNuevoProducto = (e) => {
+        e.preventDefault()
+
+        //validar formulario
+        //si no hay errores
+        //crear el nuevo producto
+        agregarProducto()
+    }
+
     return (
         <div className="row justify-content-center">
             <div className="col-md-8">
@@ -9,7 +26,7 @@ const NuevoProducto = () => {
                         <h2 className="text-cneter mb-4 font-weight-bold">
                             Complete to add new product
                         </h2>
-                        <form>
+                        <form onSubmit={submitNuevoProducto}>
                             <div className="form-group">
                                 <label>Product Name</label>
                                 <input

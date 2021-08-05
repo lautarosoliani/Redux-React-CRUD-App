fc4b986f1e4c61f9c955c5bf28e78d3856ae4939
Lautaro <lautarosoliani@hotmail.com>
2021-08-04 23:52:17 -0300

    302 onProgress

--- myapp/src/actions/productoActions.js
+++ myapp/src/actions/productoActions.js
@@ -1,11 +1,32 @@
-export const AGREGAR_PRODUCTO = "AGREGAR_PRODUCTO"
-export const AGREGAR_PRODUCTO_EXITO = "AGREGAR_PRODUCTO_EXITO"
-export const AGREGAR_PRODUCTO_ERROR = "AGREGAR_PRODUCTO_ERROR"
+import {
+    AGREGAR_PRODUCTO,
+    AGREGAR_PRODUCTO_EXITO,
+    AGREGAR_PRODUCTO_ERROR,
+} from "../types"
 
-// crear nuevos productos
+//crear nuevos productos
 
 export function crearNuevoProductoAction(producto) {
-    return () => {
-        console.log(producto)
+    return (dispatch) => {
+        dispatch(agregarProducto())
+        try {
+            dispatch(agregarProductoExito(producto))
+        } catch (error) {
+            dispatch(agregarProductoError(true))
+        }
     }
 }
+
+const agregarProducto = () => ({
+    type: AGREGAR_PRODUCTO,
+    payload: true,
+})
+
+//si el producto se guarda en la base de datos
+const agregarProductoExito = (producto) => ({
+    type: AGREGAR_PRODUCTO_EXITO,
+    payload: producto,
+})
+
+//si hubo error
+const agregarProductoError = () => ({})
